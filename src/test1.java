850ea6ca17185a55d904dad3314ce6f8444287a1
admin <admin@163.com>
2018-12-16 20:46:10 +0800

    添加了一个方法

--- src/test1.java
+++ src/test1.java
@@ -6,4 +6,13 @@ public class test1 {
         System.out.println("111111111111");
         System.out.println("111111111111");
     }
+
+    public void test(){
+        System.out.println("2222222222");
+        System.out.println("2222222222");
+        System.out.println("2222222222");
+        System.out.println("2222222222");
+        System.out.println("2222222222");
+        System.out.println("2222222222");
+    }
 }
